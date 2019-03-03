7dd4288013ec2e8abc7d1b49dff9ec9701ca9968
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-03-03 21:49:02 +0300

    Add files via upload

---
+++ task7.11/task7.11/Main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <string> 
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	std::string inputStr;
+	std::getline(std::cin, inputStr);
+
+
+	int word_counter = 1;
+	for (int i = 0; i < inputStr.length(); i++)
+	{
+		if (inputStr.at(i) == *" ")
+		{
+			word_counter++;
+		}
+	}
+	std::string *word_arr = new std::string[word_counter];
+	int words = 0;
+
+	for (int i = 0; i < inputStr.length(); i++)
+	{
+		if (inputStr.at(i) != *" ")
+		{
+			word_arr[words] += inputStr.at(i);
+		}
+		else
+		{
+			words++;
+		}
+	} 
+
+	
+
+	for (int i = 0; i < word_counter; i++)
+	{
+		if (word_arr[i].at(0) != *"t")
+		{
+			word_arr[i] = "null";
+		}
+	}
+	for (int i = 0; i < word_counter; i++)
+	{
+		for (int j = 0; j < word_counter; j++)
+		{
+			if (word_arr[i] != "null")
+			{
+				
+				if (word_arr[i] == word_arr[j] && i != j)
+				{
+					word_arr[i] = "null";
+				}
+			}
+		}
+	}
+	std::cout << "========================================================\n";
+
+
+	for (int i = 0; i < word_counter; i++)
+	{
+		if (word_arr[i] != "null")
+		{
+			std::cout << word_arr[i] << std::endl;
+		}
+	}
+
+
+
+
+
+
+	_getch();
+	return EXIT_SUCCESS;	//define EXIT_SUCCESS as 0 (in iostream)
+}
