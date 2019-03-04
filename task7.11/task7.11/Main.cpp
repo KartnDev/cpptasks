ce88edd20b20b489184b12d69932e4272136e315
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-03-04 23:40:28 +0300

    Add files via upload

--- task7.11/task7.11/Main.cpp
+++ task7.11/task7.11/Main.cpp
@@ -3,45 +3,75 @@
 #include <conio.h>
 #include <string> 
 
+//function takes string line and returns array of words(strings)
 
-std::string split_string_by_words(std::string string_name)
+std::string *split_string_by_words(std::string string_name)
 {
-	//function takes string line and returns array of words(strings)
+	
+	int word_counter = 1;
+	for (int i = 0; i < string_name.length(); i++)
+	{
+		if (string_name.at(i) == *" ")
+		{
+			word_counter++;
+		}
+	}
+
+
+	std::string *word_arr = new std::string[word_counter];
+
+	int count_of_words = 0;
+
+	for (int i = 0; i < string_name.length(); i++)
+	{
+		if (string_name.at(i) != *" ")
+		{
+			word_arr[count_of_words] += string_name.at(i);
+		}
+		else
+		{
+			count_of_words++;
+		}
+	}
 
 
 
-	return "";
+	return word_arr;
 }
 
 
 
-
-std::string sort_words_by_simbol(std::string word_arr[], char argument_simbol)	 
-
-{	/*
-	@	function takes first arg as ARRAY OF STRINGS (that strings is words) 
+	/*
+	@	function takes first arg as ARRAY OF STRINGS (that strings is words)
 	@   and second arg is flag-simbol
-	@	if word have first simbol == argument_simbol 
-	@	this word will override by 'null_string' 
-	@	after all result_arr(string) will be returned 
+	@	if word have first simbol == argument_simbol
+	@	this word will override by 'null_string'
+	@	after all result_arr(string) will be returned
 	@	result_arr have only words which starts from 'argument_simbol'
 	*/
 
+std::string sort_words_by_simbol(std::string word_arr[], char argument_simbol)	 
+
+{	
+
 
 
 	return "";
 }
 
-
+//funtion which taking file name and reading file then returning converted string
 std::string convert_text_to_string(std::string file_name)		
 {
-	//funtion which taking file name and reading file then returning converted string
+	
 
 
 	return "";
 }
 
 
+
+
+//TODO: is (simbol+word is end-word -> crash 
 
 
 
@@ -52,31 +82,11 @@ int main()
 	std::getline(std::cin, inputStr);
 
 
-	int word_counter = 1;
-	for (int i = 0; i < inputStr.length(); i++)
-	{
-		if (inputStr.at(i) == *" ")
-		{
-			word_counter++;
-		}
-	}
 
 
-	std::string *word_arr = new std::string[word_counter];
-	int words = 0;
-
-	for (int i = 0; i < inputStr.length(); i++)
-	{
-		if (inputStr.at(i) != *" ")
-		{
-			word_arr[words] += inputStr.at(i);
-		}
-		else
-		{
-			words++;
-		}
-	} 
-
+	std::string *word_arr = split_string_by_words(inputStr);
+	
+	int word_counter = word_arr->length();
 	
 
 	for (int i = 0; i < word_counter; i++)
