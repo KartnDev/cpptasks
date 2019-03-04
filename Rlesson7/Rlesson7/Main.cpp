adb454faf51b337f4048028f12817afa6f55729e
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-03-04 22:05:14 +0300

    lesson7  as 2/2 added

--- Rlesson7/Rlesson7/Main.cpp
+++ Rlesson7/Rlesson7/Main.cpp
@@ -4,6 +4,8 @@
 
 int main()
 {	
+	setlocale(LC_ALL, "ru");
+
 	//считывем строку из консоли, испульзуя getline
 	//cin недопустим, тк он будет игнорировать всё после пробела(1 слово прочтет)
 	std::string inputStr; 
@@ -23,7 +25,7 @@ int main()
 		}
 	}
 
-	//создаем массив(WordArr) типа string, куда будем записывать слова из считанной строки (inputStr)
+	//создаем (динамически выделяем) массив(WordArr) типа string, куда будем записывать слова из считанной строки (inputStr)
 	std::string *WordArr = new std::string[WordCnt]();
 
 	//переменная целочисленных беззнаковых чисел для перехода между словами (лежит от нуля до WordCount)
@@ -32,10 +34,9 @@ int main()
 	//пробегаем по всем символам строки
 	//записываем те символы WordArr(массив слов) 
 	//если встречается пробел, то перепрыгиваем на новое слово 
-	//пробелы не входят в слова
 	for (unsigned i = 0; i < inputStr.length(); i++)
 	{
-		if (inputStr.at(i) == *" ")
+		if (inputStr.at(i) == *" ") //эквивалентно записи inputStr[i]
 		{
 			label++;
 		}
@@ -48,6 +49,48 @@ int main()
 	//начинаем с WordCount и заканчиваем на 0
 	for (unsigned i = WordCnt; i > 0; i--)
 		std::cout << WordArr[i - 1] << " ";
+
+	//удалить экземпляр массива слов 
+	delete[] WordArr;
+
+	system("pause"); //конец первый половины
+	std::cout << "-------------------------------------------------\n";
 	
+	
+	
+	char *str = new char[inputStr.length()];
+	std::string *words = new std::string[WordCnt];
+	
+
+
+	for (int i = 0; i < inputStr.length(); i++)
+	{
+		str[i] = inputStr[i];
+	}
+	
+	char *next_token, *token1 = strtok_s(str, " ", &next_token);
+
+	for(unsigned counter = 0; token1 != NULL; counter++)
+	{
+		if (WordCnt == counter) break;
+		words[counter] = token1; // токен без ссылки * имеет значение всего слова, а со ссылкой лишь 1ый символ
+		std::cout  << "token at (" << counter << ") : "<< token1 <<std::endl;
+		token1 = strtok_s(NULL, " ", &next_token);
+	}
+	std::cout << "-------------------------------------------------\n";
+	std::cout << "result:\n";
+
+	for (unsigned i = WordCnt; i > 0; i--)
+		std::cout << words[i - 1] << " ";
+
+	system("pause");
+
+
+
+
+
+	delete[] str;
+	delete[] words;
 	_getch();
+	return EXIT_SUCCESS;
 }
