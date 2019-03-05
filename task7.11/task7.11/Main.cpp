35b273d15e722f7c3328acdc7fe99b47fca77dc9
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-03-05 14:41:21 +0300

    Add files via upload

--- task7.11/task7.11/Main.cpp
+++ task7.11/task7.11/Main.cpp
@@ -7,7 +7,6 @@
 
 std::string *split_string_by_words(std::string string_name)
 {
-	
 	int word_counter = 1;
 	for (int i = 0; i < string_name.length(); i++)
 	{
@@ -16,7 +15,6 @@ std::string *split_string_by_words(std::string string_name)
 			word_counter++;
 		}
 	}
-
 
 	std::string *word_arr = new std::string[word_counter];
 
@@ -34,8 +32,6 @@ std::string *split_string_by_words(std::string string_name)
 		}
 	}
 
-
-
 	return word_arr;
 }
 
@@ -50,13 +46,56 @@ std::string *split_string_by_words(std::string string_name)
 	@	result_arr have only words which starts from 'argument_simbol'
 	*/
 
-std::string sort_words_by_simbol(std::string word_arr[], char argument_simbol)	 
-
+std::string *sort_words_by_simbol(std::string word_arr[], char argument_simbol)	 
 {	
 
+	int word_counter = word_arr->length();
 
 
-	return "";
+	for (int i = 0; i < word_counter; i++)
+	{
+		if (word_arr[i].at(0) != argument_simbol)
+		{
+			word_arr[i] = "null";
+		}
+	}
+	for (int i = 0; i < word_counter; i++)
+	{
+		for (int j = 0; j < word_counter; j++)
+		{
+			if (word_arr[i] != "null")
+			{
+
+				if (word_arr[i] == word_arr[j] && i != j)
+				{
+					word_arr[i] = "null";
+				}
+			}
+		}
+	}
+
+	int result_words_counter=0;
+
+	for (int i = 0; i < word_counter; i++)
+	{
+		if (word_arr[i] != "null")
+		{
+			result_words_counter++;
+		}
+	}
+	std::string *resulting_string = new std::string[word_counter];
+
+	for (int i=0, counter=0; i < word_counter; i++)
+	{
+		if (word_arr[i] != "null")
+		{
+			resulting_string[counter] = word_arr[i]; 
+			counter++;
+		}
+	}
+
+
+	return resulting_string;
 }
 
 //funtion which taking file name and reading file then returning converted string
@@ -78,51 +117,25 @@ std::string convert_text_to_string(std::string file_name)
 int main()
 {
 	setlocale(LC_ALL, "Ru");
+
 	std::string inputStr;
+	char simbol;
+
+	std::cout << "Enter the string\n";
 	std::getline(std::cin, inputStr);
 
+	std::cout << "Enter the simbol\n";
+	std::cin >> simbol;
 
-
-
-	std::string *word_arr = split_string_by_words(inputStr);
+	std::string *end_str = sort_words_by_simbol(split_string_by_words(inputStr), simbol);
 	
-	int word_counter = word_arr->length();
-	
-
-	for (int i = 0; i < word_counter; i++)
+	for(int i =0; i < end_str->length();i++)
 	{
-		if (word_arr[i].at(0) != *"t")
-		{
-			word_arr[i] = "null";
-		}
-	}
-	for (int i = 0; i < word_counter; i++)
-	{
-		for (int j = 0; j < word_counter; j++)
-		{
-			if (word_arr[i] != "null")
-			{
-				
-				if (word_arr[i] == word_arr[j] && i != j)
-				{
-					word_arr[i] = "null";
-				}
-			}
-		}
-	}
-	std::cout << "========================================================\n";
-
-
-	for (int i = 0; i < word_counter; i++)
-	{
-		if (word_arr[i] != "null")
-		{
-			std::cout << word_arr[i] << std::endl;
-		}
+		std::cout << end_str[i] << std::endl;
 	}
 
-	delete[] word_arr;
 
+	delete[] end_str;
 	
 	_getch();
 	return EXIT_SUCCESS;	//define EXIT_SUCCESS as 0 (in iostream)
