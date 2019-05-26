57ebe42dfd4918b7bc22f3a38f189af133eca559
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-05-26 18:36:52 +0300

    Add files via upload

--- RLesson12/RLesson12/Main.cpp
+++ RLesson12/RLesson12/Main.cpp
@@ -1,3 +1,10 @@
+/*наследование : платеж(суперкласс) -> 
+-наследники:
+-платеж по карте, 
+-платеж по кэшу(наличными)
+по карте имеет реквизиты
+по наличными сдачу и те
+*/
 #include <iostream>
 using namespace std;
 
@@ -7,10 +14,21 @@ constexpr auto LINE = "=========================================================
 class Buissness_math
 {
 public:
-	 int purchase(int price, int wallet_bank)
-	 {
+	Buissness_math()
+	{
+		wallet = 0;
+		spent_money = 0;
+	}
+	int get_spent_money()
+	{
+		return spent_money;
+	}
+protected:
+	int purchase_or_pay(int price, int wallet_bank)
+	{
 		if (price < wallet_bank)
 		{
+			spent_money += price;
 			return wallet_bank - price;
 		}
 		else
@@ -18,10 +36,7 @@ public:
 			purchase_exception(price, wallet_bank);
 			return -1;
 		}
-	 }
-
-private:
-
+	}
 	void purchase_exception(int price, int wallet_bank)
 	{
 		cout << LINE;
@@ -29,16 +44,18 @@ private:
 		cout << "Банк вашего кошелька (" << wallet_bank << ") меньше чем цена совершаемой транзакции (" << price << ")\n";
 		cout << LINE;
 	}
-protected:
-
 	void deposit(int& inc_money, int& bank)
 	{
 		bank += inc_money;
 	}
-	void check_money(int bank)	// рид - онли 
+	void check_money(int &bank)	// рид - онли 
 	{
 		cout << "Ваш баланс денег:" << bank << endl;
 	}
+protected:
+	int wallet;
+private:
+	int spent_money;
 };
 
 
@@ -46,47 +63,136 @@ protected:
 class Cash : public Buissness_math
 {
 public:
-	
+
 	Cash()
 	{
-		wallet_money = 0;
+		wallet = 0;
 	}
-
+	void purchase(int price)
+	{
+		if (price < Buissness_math::wallet)
+		{
+			wallet = purchase_or_pay(price, wallet);
+		}
+		else
+		{
+			purchase_exception(price, wallet);
+		}
+	}
 	void add_money(int inc_money)
 	{
-		deposit(inc_money, wallet_money);
+		deposit(inc_money, wallet);
 		cout << "Вы положили в кошелек:" << inc_money << "денег\n";
 	}
-	void check_money()
+	void view_money()
 	{
-
+		check_money(wallet);
 	}
 
-private:
-	int wallet_money;
 };
 
 class Card_pay : public Buissness_math
 {
 public:
-	void send_money(&)
+	void pay(int price)
 	{
-
+		if (price < wallet)
+		{
+			wallet = purchase_or_pay(price, wallet);
+		}
+		else
+		{
+			purchase_exception(price, wallet);
+		}
+	}
+	void send_money(Card_pay& other_card, int money)
+	{
+		if (money < wallet)
+		{
+			other_card.add_money(money);
+			wallet -= money;
+		}
+		else
+		{
+			purchase_exception(money, wallet);
+		}
+	}
+	void add_money(int money)
+	{
+		deposit(money, wallet);
+	}
+	void view_money()
+	{
+		check_money(wallet);
 	}
 	Card_pay()
 	{
-		card_money = 0;
+		wallet = 0;
 	}
-
-private:
-	int card_money;
 };
 
 
 int main()
 {
 	setlocale(LC_ALL, "Ru");
+	/*
+	Card_pay eletracard, sbercard;
+	cout << "Добавим на сбербанк :";
+	sbercard.add_money(1000);
+	sbercard.view_money();
+	cout << LINE;
 
+	cout << "Добавим на электракарту :";
+	eletracard.add_money(500);
+	eletracard.view_money();
+	cout << LINE;
+
+	cout << "отправим 100 на електракарту с карты сбербанка: \n";
+	sbercard.send_money(eletracard, 100);
+	cout << LINE;
+	cout << "На сбербанке: ";
+	sbercard.view_money();
+	cout << endl;
+	cout << "На электрокате: ";
+	eletracard.view_money();
+	cout << LINE;
+
+
+	cout << "заплатим за покупку с сбербанка на 500\n";
+	sbercard.pay(500);
+	cout << LINE;
+	cout << "На сбербанке осталось: ";
+	sbercard.view_money();
+	cout << LINE;
+
+	system("pause");
+	cout << "\n\n\n\n\n\n";
+	Cash WALLET;
+	WALLET.add_money(1000);
+	WALLET.view_money();
+
+	cout << LINE;
+	cout << "купим на 600: \n";
+	WALLET.purchase(600);
+	WALLET.view_money();
+	*/
+
+	Buissness_math *sells = new Buissness_math[10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		(i % 2) == 0 ? sells[i] = Card_pay() : sells[i] = Cash();
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		if (i % 2)
+		{
+			(Card_pay)sells[i].Card_pay::add_money(1000);
+		}
+	}
+
+
+	//сколько всего выплат было?
 
 
 	system("pause");
