8a50682d687dcbb64d47160ac975c0f89b259f89
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-03-08 23:54:39 +0300

    Add files via upload

---
+++ FourierSeries/FourierSeries/FourierSeries.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <valarray>
+#include <cmath>
+
+//using cmplx = complex<double>;
+//using csignal = vector<cmplx>;
+
+
